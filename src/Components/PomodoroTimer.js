d4ef3997c755b4b2c52f27e19c351265f25174c0
Echo Sit <echosit@Echos-MacBook-Air.local>
2021-02-08 10:51:22 -0800

    create ReactJS app

---
+++ src/Components/PomodoroTimer.js
@@ -0,0 +1,10 @@
+const PomodoroTimer = () => {
+    return (
+      <div>
+            <h1>Time</h1>
+            <button>Start Time</button>
+      </div>
+    );
+}
+
+export default PomodoroTimer; 
