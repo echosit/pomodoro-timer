4c2d1fd9f73a818e49adda8598dae844ac1b4f24
Echo Sit <echosit@Echos-MacBook-Air.local>
2021-02-09 12:08:59 -0800

    add hooks to make seconds increase every second

--- src/Components/PomodoroTimer.js
+++ src/Components/PomodoroTimer.js
@@ -1,10 +1,23 @@
-const PomodoroTimer = () => {
-    return (
-      <div>
-            <h1>Time</h1>
-            <button>Start Time</button>
-      </div>
-    );
-}
+import React, { useState, useEffect } from 'react';
 
-export default PomodoroTimer; 
+export default function Timer() {
+// useState Hook to set default value of seconds to 0
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>{seconds} Seconds</h1>
+      <button>Start Timer</button>
+    </div>
+  );
+}
