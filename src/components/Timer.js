19b1351208e9ece46f58cd3664f6a526224f1793
Echo Sit <echo.sit@comcast.net>
2021-02-25 11:07:16 -0800

    set state of focusTime

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,7 +6,9 @@ export default function Timer() {
   const [hours, setHours] = useState(0);
   const [start, setStart] = useState(false);
   const [button, setButton] = useState("Start");
-  const [pomodoroTracker, setpomodoroTracker] = useState(0);
+  const [pomodoroTracker, setPomodoroTracker] = useState(0);
+  const [focusTime, setFocusTime] = useState(0);
+
 
   //add 1 to seconds after second
   useEffect(() => {
@@ -38,7 +40,7 @@ export default function Timer() {
   //Reset value of minutes to 0 after 60 and add 1 to hours
   if (minutes === 60) {
     setMinutes(0);
-    setHours(hours + 1);
+    setHours(hours + 1); 
   }
 
   //Pomodoro Time Interval
@@ -47,14 +49,14 @@ export default function Timer() {
     setMinutes(0); //sets minutes 
     setStart(false); //stops timer
     setButton("Start"); //button is on Start
-    setpomodoroTracker(pomodoroTracker => pomodoroTracker + 1); //add 1 to pomodoroTracker
+    setPomodoroTracker(pomodoroTracker => pomodoroTracker + 1); //add 1 to pomodoroTracker
   } 
-
   
   return (
     <div>
       <h1>Pomodoro: {pomodoro} minutes</h1>
       <p>You have Completed {pomodoroTracker} Pomodoros</p>
+      <p>Total Time Worked: {focusTime} Minutes</p>
       <h1>{ hours < 10 && '0' }{hours}:{ minutes < 10 && '0' }{minutes}:{ seconds < 10 && '0' }{seconds}</h1>
       <button onClick={handleClick}>{button}</button>
     </div>
