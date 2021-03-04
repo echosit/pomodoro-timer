e1cb81637081d4cee888dd822d69fbdfe53e1d49
Echo Sit <echo.sit@comcast.net>
2021-03-04 15:05:43 -0800

    add min and max values for input

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,7 +5,7 @@ export class Input extends React.Component {
   constructor(props) {
     super(props);
     
-    this.state = { userInput: 25 };
+    this.state = { userInput: 25 }; //default value of input to 25
     
     this.handleUserInput = this.handleUserInput.bind(this);
   }
@@ -17,7 +17,7 @@ export class Input extends React.Component {
   render() {
     return (
       <div>
-        <input type="number" onChange={this.handleUserInput} value={this.state.userInput} />
+        <input type="number" onChange={this.handleUserInput} value={this.state.userInput} max={60} min={1}/>
         <Timer pomodoro={this.state.userInput}/> {/* Set props.pomodoro to input value */}
       </div>
     );
